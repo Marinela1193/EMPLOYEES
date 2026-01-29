efbec18e74b57ff570a45d87edddfd6e86c884b1
Marinela1193 <marpersuar2@alu.edu.gva.es>
2026-01-29 21:32:27 +0100

    validation

--- src/main/java/org/example/employees2/models/dto/EmployeeDTO.java
+++ src/main/java/org/example/employees2/models/dto/EmployeeDTO.java
@@ -1,5 +1,10 @@
 package org.example.employees2.models.dto;
 
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class EmployeeDTO {
 
     private int empno;
@@ -16,6 +21,10 @@ public class EmployeeDTO {
 
     public EmployeeDTO() {}
 
+    @Basic
+    @NotEmpty(message = "The employee number cannot be blank")
+    @Size(min = 1, message = "Name size must be minimum 1")
+    @Column(name = "empno", nullable = false)
     public int getEmpno() {
         return empno;
     }
@@ -23,7 +32,9 @@ public class EmployeeDTO {
     public void setEmpno(int empno) {
         this.empno = empno;
     }
-
+    @Basic
+    @Size(min = 2,max = 10, message = "Name size must be between 2 and 10")
+    @Column(name = "ename", nullable = true, length = 10)
     public String getEname() {
         return ename;
     }
@@ -32,6 +43,9 @@ public class EmployeeDTO {
         this.ename = ename;
     }
 
+    @Basic
+    @Size(min = 2,max = 9, message = "Name size must be between 2 and 9")
+    @Column(name = "job", nullable = true, length = 9)
     public String getJob() {
         return job;
     }
@@ -39,7 +53,10 @@ public class EmployeeDTO {
     public void setJob(String job) {
         this.job = job;
     }
-
+    @Basic
+    @NotEmpty(message = "The department number cannot be blank")
+    @Size(min = 1, message = "Name size must be minimum 1")
+    @Column(name = "deptno", nullable = true)
     public int getDeptno() {
         return deptno;
     }
