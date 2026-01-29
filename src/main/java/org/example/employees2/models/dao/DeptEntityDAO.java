6e8b3c242b66773e565ec0f974baa21f86eab229
Marinela1193 <marpersuar2@alu.edu.gva.es>
2026-01-29 19:57:16 +0100

    Implementamos DAOS

--- src/main/java/org/example/employees2/models/dao/DeptEntityDAO.java
+++ src/main/java/org/example/employees2/models/dao/DeptEntityDAO.java
@@ -2,12 +2,48 @@ package org.example.employees2.models.dao;
 
 import org.example.employees2.models.entities.DeptEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface DeptEntityDAO extends JpaRepository<DeptEntity, Integer> {
+public interface DeptEntityDAO extends CrudRepository<DeptEntity, Integer> {
     static Optional<DeptEntity> findById(DeptEntity deptno) {
         return null;
     }
 
+    @Override
+    <S extends DeptEntity> S save(S entity);
+
+    @Override
+    <S extends DeptEntity> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    Optional<DeptEntity> findById(Integer integer);
+
+    @Override
+    boolean existsById(Integer integer);
+
+    @Override
+    Iterable<DeptEntity> findAll();
+
+    @Override
+    Iterable<DeptEntity> findAllById(Iterable<Integer> integers);
+
+    @Override
+    long count();
+
+    @Override
+    void deleteById(Integer integer);
+
+    @Override
+    void delete(DeptEntity entity);
+
+    @Override
+    void deleteAllById(Iterable<? extends Integer> integers);
+
+    @Override
+    void deleteAll(Iterable<? extends DeptEntity> entities);
+
+    @Override
+    void deleteAll();
 }
--- src/main/java/org/example/employees2/models/dao/EmployeeEntityDAO.java
+++ src/main/java/org/example/employees2/models/dao/EmployeeEntityDAO.java
@@ -2,23 +2,68 @@ package org.example.employees2.models.dao;
 import org.example.employees2.models.dto.EmployeeDTO;
 import org.example.employees2.models.entities.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface EmployeeEntityDAO extends JpaRepository<EmployeeEntity, Integer> {
-    /*List<EmployeeEntity> findByDeptnoGreaterThan(int deptno);
-    List<EmployeeEntity> findByDeptnoLessThan(int deptno);
+public interface EmployeeEntityDAO extends CrudRepository<EmployeeEntity, Integer> {
 
-    Object findAll();
+    @Override
+    default <S extends EmployeeEntity> S save(S entity) {
+        return null;
+    }
 
-    Optional<EmployeeEntity> findById(int id);
+    @Override
+    default <S extends EmployeeEntity> Iterable<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
 
-    EmployeeEntity save(EmployeeEntity employee);
+    @Override
+    default Optional<EmployeeEntity> findById(Integer integer) {
+        return Optional.empty();
+    }
 
-    void deleteById(int id);*/
+    @Override
+    default Iterable<EmployeeEntity> findAll() {
+        return null;
+    }
+
+    @Override
+    default Iterable<EmployeeEntity> findAllById(Iterable<Integer> integers) {
+        return null;
+    }
+
+    @Override
+    default long count() {
+        return 0;
+    }
+
+    @Override
+    default void deleteById(Integer integer) {
+
+    }
+
+    @Override
+    default void delete(EmployeeEntity entity) {
+
+    }
+
+    @Override
+    default void deleteAllById(Iterable<? extends Integer> integers) {
+
+    }
+
+    @Override
+    default void deleteAll(Iterable<? extends EmployeeEntity> entities) {
+
+    }
+
+    @Override
+    default void deleteAll() {
+
+    }
 
     boolean existsById(Integer id);
 
-    boolean existsById(EmployeeDTO employeeDTO);
 }
