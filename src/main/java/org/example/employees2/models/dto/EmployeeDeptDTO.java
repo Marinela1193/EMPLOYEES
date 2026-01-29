d39c52a64ad1d1c4bf7d239801473683879f1941
Marinela1193 <marpersuar2@alu.edu.gva.es>
2026-01-29 19:18:17 +0100

    create DTO with information given

--- src/main/java/org/example/employees2/models/dto/EmployeeDeptDTO.java
+++ src/main/java/org/example/employees2/models/dto/EmployeeDeptDTO.java
@@ -1,4 +1,60 @@
 package org.example.employees2.models.dto;
 
 public class EmployeeDeptDTO {
+    private int deptno;
+    private String ename;
+    private String dloc;
+    private String employeeNumber;
+    private int superior;
+
+    public EmployeeDeptDTO(int deptno, String ename, String dloc, String employeeNumber, int superior) {
+        this.deptno = deptno;
+        this.ename = ename;
+        this.dloc = dloc;
+        this.employeeNumber = employeeNumber;
+        this.superior = superior;
+    }
+
+    public EmployeeDeptDTO() {
+    }
+
+    public int getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(int deptno) {
+        this.deptno = deptno;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getDloc() {
+        return dloc;
+    }
+
+    public void setDloc(String dloc) {
+        this.dloc = dloc;
+    }
+
+    public String getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(String employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public int getSuperior() {
+        return superior;
+    }
+
+    public void setSuperior(int superior) {
+        this.superior = superior;
+    }
 }
